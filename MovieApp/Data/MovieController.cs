e10be3700a83a3f91fc7c86f966e513f1991c036
Artis <mundurs.artis@gmail.com>
2022-05-18 13:54:38 +0200

    movie page

---
+++ MovieApp/Data/MovieController.cs
@@ -0,0 +1,17 @@
+﻿namespace MovieApp.Data;
+
+public class MovieController
+{
+    private MovieDBO _movieDbo;
+
+    public MovieController()
+    {
+        _movieDbo = new MovieDBO();
+    }
+
+    public async Task<Dictionary<String, String>> getMovies(String movieId)
+    {
+        Dictionary<String, String> movies = _movieDbo.getMovieInfo(movieId);
+        return movies;
+    }
+}
