e55355633de8af09fbd71d32bfb44bea2ca98d44
Dan Moskalchuck <denm.2206@gmail.com>
2022-05-23 17:13:03 +0200

    movies ordered by rating + rating displayed

--- MovieApp/Data/Models/Movie.cs
+++ MovieApp/Data/Models/Movie.cs
@@ -9,6 +9,10 @@ public class Movie
     [NotNull] public int id { get; set; }
     [AllowNull] public string title { get; set; }
     [AllowNull] public int year { get; set; }
+    
+    [AllowNull] public float rating { get; set; }
+    [AllowNull] public int votes { get; set; }
+    
     [AllowNull] public Person Actor { get; set; }
     [AllowNull] public Person Director { get; set; }
     
